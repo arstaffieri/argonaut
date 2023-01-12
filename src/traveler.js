351b4b2b0d22163efaa822da1d6624099f3a88ce
Angie Staffieri <angie.staffieri@gmail.com>
2023-01-11 19:49:17 -0700

    Start of the traveler class and test

---
+++ src/traveler.js
@@ -0,0 +1,10 @@
+class Traveler {
+    constructor(peopleData) {
+        this.id = peopleData.id
+        this.name = peopleData.name
+        this.travelerType = peopleData.travelerType
+    }
+}
+
+
+export default Traveler
